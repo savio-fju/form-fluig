d2b41af0720d35f7401cd218f9af03d375055f71
Sávio Pereira <saviofju7@gmail.com>
2021-06-15 18:47:29 -0300

    Update JS

--- src/nf.js
+++ src/nf.js
@@ -126,25 +126,33 @@ function chamar() {
 
 // -------------------------------- ADD PRODUTOS  ---------------------------------------------
 
-function AddTableRow() {
-    var newRow = $("<tr>");
-    var cols = "";
+function AddTableRow(index) {
+    let product = {
+        code: ['000001', '000002', '000003'],
+        description: ["Notebook", "Teclado", "Mouse"],
+        amount: [3000, 150, 80],
+        tax: [15, 10, 5]
+    }
 
-    cols += '<td><input type="text" name="code[]" id="code" class="code" placeholder="C&oacute;digo Produto">        </td>';
-    cols += '<td><input type="text" name="description[]" id="description" class="description" placeholder="Descri&ccedil;&atilde;o Produto"></td>';
-    cols += '<td><input onblur="calcula(this);" type="text" name="quantity[]" id="quantity" class="quantity" placeholder="0,00"></td>';
-    cols += '<td><input onblur="calcula(this);" type="text" name="amount[]" id="amount" class="amount" placeholder="0,00"></td>';
-    cols += '<td><input type="text" name="total[]" id="total" class="total" placeholder="0,00"></td>';
-    cols += '<td><input type="text" name="tax[]" id="tax" class="tax" placeholder="0,00"></td>';
-    cols += '<td>';
-    cols += '<button id="remove" onclick="RemoveTableRow(this)" type="button">Remover</button>';
-    cols += '</td>';
+    const html = `
+    <td>
+    <input type="text" name="code" id="code" class="code" value="${product.code[0]}" placeholder="C&oacute;digo Produto"></td>
+    <td><input type="text" name="description" id="description" class="description" value="${product.description[0]}" placeholder="Descri&ccedil;&atilde;o Produto"></td>
+    <td><input onblur="calcula(this);" type="text" name="quantity" id="quantity" class="quantity" value="" placeholder="0,00"></td>
+    <td><input onblur="calcula(this);" type="text" name="amount" id="amount" class="amount" value="${product.amount[0]}" placeholder="0,00"></td>
+    <td><input type="text" name="total" id="total" class="total" value=""  placeholder="0,00"></td>
+    <td><input type="text" name="tax" id="tax" class="tax" value="${product.tax[0]}" placeholder="0,00"></td>
+    <td><button id="remove" onclick="RemoveTableRow(this)" type="button">Remover</button></td>
+    `
 
-    newRow.append(cols);
-    $("#data-table").append(newRow);
+    const tb = document.querySelector('#data-table tbody')
+    const tr = document.createElement('tr')
+    tr.innerHTML = html
+    tr.dataset.index = index
+    tb.appendChild(tr)
 
-    return false;
 }
+
 
 // -------------------------------- REMOVER PRODUTOS  ---------------------------------------------
 
@@ -160,155 +168,14 @@ function RemoveTableRow(item) {
 
 // -------------------------------- CALCULAR O SUBTOTAL  ---------------------------------------------
 
-function calc(item) {
 
-    amount = document.getElementById("amount").value;
-    quantity = document.getElementById("quantity").value;
-
-    total = amount * quantity;
-
-    document.getElementById("total").value = total;
-}
-
-function calc2(item) {
-
-    amount = document.getElementById("amount2").value;
-    quantity = document.getElementById("quantity2").value;
-
-    total = amount * quantity;
-
-    document.getElementById("total2").value = total;
-}
-
-function calc3(item) {
-
-    amount = document.getElementById("amount3").value;
-    quantity = document.getElementById("quantity3").value;
-
-    total = amount * quantity;
-
-    document.getElementById("total3").value = total;
-
-    calcQuantity()
-    maskMoney()
-}
 
 // -------------------------------- CALCULAR O TOTAL  ---------------------------------------------
 
 
-function calcQuantity() {
-
-
-    quantity = document.getElementById("quantity").value;
-    quantity2 = document.getElementById("quantity2").value;
-    quantity3 = document.getElementById("quantity3").value;
-
-    total = Number(quantity) + Number(quantity2) + Number(quantity3);
-
-    document.getElementById("total-quantity").value = total;
-
-    calcTax()
-    calcTotal()
-}
-
-function calcTotal() {
-
-    total = document.getElementById("total").value;
-    total2 = document.getElementById("total2").value;
-    total3 = document.getElementById("total3").value;
-
-    soma = Number(total) + Number(total2) + Number(total3);
-
-    document.getElementById("total-amount").value = soma;
-}
-
-function calcTax() {
-
-    tax = document.getElementById("tax").value;
-    tax2 = document.getElementById("tax2").value;
-    tax3 = document.getElementById("tax3").value;
-
-    soma = Number(tax) + Number(tax2) + Number(tax3);
-    total = soma + String("%")
-
-    document.getElementById("total-tax").value = total;
-
-}
-
-// --------------------------------ARRAY DOS PRODUTOS  ---------------------------------------------
-
-let product = {
-    code: ['000001', '000002', '000003'],
-    description: ["Notebook", "Teclado", "Mouse"],
-    amount: [3000, 150, 80],
-    tax: [15, 10, 5]
-}
-
-document.getElementById("code").value = product.code[0];
-document.getElementById("code2").value = product.code[1];
-document.getElementById("code3").value = product.code[2];
-
-document.getElementById("description").value = product.description[0];
-document.getElementById("description2").value = product.description[1];
-document.getElementById("description3").value = product.description[2];
-
-document.getElementById("amount").value = product.amount[0];
-document.getElementById("amount2").value = product.amount[1];
-document.getElementById("amount3").value = product.amount[2];
-
-document.getElementById("tax").value = product.tax[0];
-document.getElementById("tax2").value = product.tax[1];
-document.getElementById("tax3").value = product.tax[2];
 
 
 // -------------------------------- MASK MONEY  ---------------------------------------------
 
-function maskMoney() {
 
-    var valor = document.getElementById("amount").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("amount").value = valorFormatado;
-
-    var valor = document.getElementById("total").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("total").value = valorFormatado;
-
-
-    var valor = document.getElementById("amount2").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("amount2").value = valorFormatado;
-
-    var valor = document.getElementById("total2").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("total2").value = valorFormatado;
-
-
-    var valor = document.getElementById("amount3").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("amount3").value = valorFormatado;
-
-    var valor = document.getElementById("total3").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("total3").value = valorFormatado;
-
-
-    var valor = document.getElementById("total-amount").value;
-    //Valor com cifrão
-    var valorFormatado = Number(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    console.log(valorFormatado);
-    document.getElementById("total-amount").value = valorFormatado;
-
-}
 
